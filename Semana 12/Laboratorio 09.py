c4b96799ca338d1661a3d126d231c910288cdae6
vale101204 <157128155+vale101204@users.noreply.github.com>
2024-04-05 08:12:56 -0600

    Add files via upload

--- Semana 12/Laboratorio 09.py
+++ Semana 12/Laboratorio 09.py
@@ -7,7 +7,33 @@
 
 #ejericio 2: Pedir al usuario un numero e indicar si es positivo o negtivo
 
-numero = int(input("Ingrese un numero: "))
-if numero <0:
-    print("Numero negativo")
-else: print("Numero positivo")
+# numero = int(input("Ingrese un numero: "))
+# if numero <0:
+#     print("Numero negativo")
+# else: print("Numero positivo")
+
+# Ejercicio 3: Utilizando la estructura switch solicite un numero y si este esta dentro del rango de 1 a 5 imprima el numero en letras, si se ingresa un numero diferente debe imprimirse "numero no definido"
+
+# numero = int(input("Ingrese un numero:"))
+# match numero:
+#     case 1:
+#         print("uno")
+#     case 2: 
+#         print("dos")
+#     case 3: 
+#         print("tres")
+#     case 4: 
+#         print("cuatro")
+#     case 5: 
+#         print("cinco")
+#     case _: 
+#         print("no existe")
+
+# ejercicio 4: Escribe un programa que imprima la tabla de multiplicar del numero 2
+
+# print("Tabla de multiplicar del numeoro 2")
+
+# i = 1
+# while i <= 10:
+#     print("2 x " + str(i)+ (" = ") + str(2*i))
+#     i += 1
