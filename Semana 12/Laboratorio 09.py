bfd987dcca183b6d7095ce8dd98d8f1d122d8e56
vale101204 <157128155+vale101204@users.noreply.github.com>
2024-04-05 08:11:06 -0600

    Add files via upload

---
+++ Semana 12/Laboratorio 09.py
@@ -0,0 +1,13 @@
+#ejercicio 1: pedir el usuario su edad y decirle si es mayor de edad  #type: ignore
+
+# edad = int(input("ingrese su edad: "))
+# if edad >= 18:
+#     print("Debe tramitar su DPI")
+# else: print("Usted aun no es mayor de edad. Tramite de DPI no disponible")
+
+#ejericio 2: Pedir al usuario un numero e indicar si es positivo o negtivo
+
+numero = int(input("Ingrese un numero: "))
+if numero <0:
+    print("Numero negativo")
+else: print("Numero positivo")
