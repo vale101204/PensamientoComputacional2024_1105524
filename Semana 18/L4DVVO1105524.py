cc06f0b8dd6c8bc9f08739c71a5d4937122f18f5
vale101204 <157128155+vale101204@users.noreply.github.com>
2024-05-17 08:21:15 -0600

    Add files via upload

---
+++ Semana 18/L4DVVO1105524.py
@@ -0,0 +1,39 @@
+# Ejercicio 1
+
+# precios = [50, 75, 46, 22, 80, 65, 8]
+# def obtener_mayor_precio(lista):
+#     mayor = lista [0]
+#     for precio in lista:
+#         if precio < mayor:
+#             mayor = precio 
+#             return mayor
+#         def obtener_menor_precio(lista):
+#             menor = lista [0]
+#             for precio in lista:
+#                 if precio < menor:
+#                     menor = precio
+#                     return menor 
+#                 mayor_precio =
+#                 obtener_mayor_precio(precios)
+#                 menor_precio=
+#                 obtener_menor_precio(precios)
+#                 print("el mayor precio es:", mayor_precio)
+#                 print("el mayor precio es:", menor_precio)
+
+
+# Ejercicio 2
+
+# lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# print(lista)
+# for x in range(len(lista)-1, -1, -1):
+#     if(x%3==0 and x !=0):
+#         lista.pop(x)
+# print(lista)
+
+
+# Ejercicio 3
+
+# palabra = ['agua', 'cocacola', 'jamaica', 'horchata', 'sprite', '1988', 'fanta', 'uva']
+# palabra=input("introduce una palabra: ")
+# conteo = palabra.count(palabra)
+# print(f"La palabra '(palabra)'aparece '(conteo)' veces en la lista")
